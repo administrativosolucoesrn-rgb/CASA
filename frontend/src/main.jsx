9779219c35d9ad4d3936f0aab46b7d5917c314b7
administrativosolucoesrn-rgb <administrativo.solucoes.rn@gmail.com>
2026-04-01 22:35:56 -0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AdminPage from "./AdminPage";
+import SorteioPage from "./SorteioPage";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/admin" replace />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/sorteio/:slug" element={<SorteioPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
